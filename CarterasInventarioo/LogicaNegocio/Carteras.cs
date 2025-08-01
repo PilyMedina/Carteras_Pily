c4d53f2bccc4c788fd0412de840733d6fdef5b4f
Pilar Medina <pilarcamilmedinagarcia@gmail.com>
2025-08-01 06:15:11 -0400

    Added .gitignore and project files

---
+++ CarterasInventarioo/LogicaNegocio/Carteras.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarterasInventarioo.LogicaNegocio
+{
+    internal class Carteras
+    {
+        public int Id { get; set; }
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public decimal Precio { get; set; }
+        public int Stock { get; set; }
+    }
+}
